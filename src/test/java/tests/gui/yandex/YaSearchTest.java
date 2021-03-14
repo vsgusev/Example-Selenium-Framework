42cadfb87741f6f31a57cdb3fac23aa81189b60f
vsgusev <gusevvs143@gmail.com>
2021-03-14 12:38:49 +0300

    add suite, params annotation

--- src/test/java/tests/gui/yandex/YaSearchTest.java
+++ src/test/java/tests/gui/yandex/YaSearchTest.java
@@ -2,19 +2,31 @@ package tests.gui.yandex;
 
 import forms.yandex.YaResultPage;
 import forms.yandex.YaStartPage;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import webdriver.BaseTest;
 
+import static org.testng.Assert.assertTrue;
+
 public class YaSearchTest extends BaseTest {
 
-    @Test
+    private String searchText;
+
+    @BeforeTest
+    @Parameters({"searchText"})
+    public void readParams(String searchText) {
+        this.searchText = searchText;
+    }
+
+    @Test(description = "YA-1 Проверка поиска")
     public void SearchTest() {
 
         YaStartPage yaStartPage = new YaStartPage();
-        yaStartPage.fillSearchField("someText");
+        yaStartPage.fillSearchField(searchText);
         yaStartPage.searchBtnClick();
         YaResultPage yaResultPage = new YaResultPage();
-        yaResultPage.searchBtnIsPresent();
+        assertTrue(yaResultPage.searchBtnIsPresent());
 
     }
 
